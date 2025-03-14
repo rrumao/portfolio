082c4db5e80b568e172252797504507827ccc216
Ryan Rumao <ryanrumao4@gmail.com>
2025-03-14 17:04:04 -0500

    Navbar Completed

--- portfolio/assets/assets.js
+++ portfolio/assets/assets.js
@@ -1,9 +1,17 @@
 import logo from './logo.png';
 import logo_dark from './logo-dark.png';
 import bg_header from './header-bg-color.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import moon_icon from './moon_icon.png';
+import close_black from './close-black.png';
 
 export const assets = {
     logo,
     logo_dark,
     bg_header,
+    menu_black,
+    menu_white,
+    moon_icon,
+    close_black
 };
