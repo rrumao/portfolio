12ffafcaa37af91b153263030cfb8fa432b2ca72
Ryan Rumao <ryanrumao4@gmail.com>
2025-03-23 20:18:06 -0500

    Initial Layout

--- portfolio/app/layout.js
+++ portfolio/app/layout.js
@@ -1,26 +1,26 @@
-import { Outfit, Ovo } from "next/font/google";
+import { Outfit, Ovo} from "next/font/google";
 import "./globals.css";
 
 const outfit = Outfit({
-  subsets: ["latin"],
-  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
 });
 
 const ovo = Ovo({
-  subsets: ["latin"],
-  weight: ["400"],
+  subsets: ["latin"], weight: ["400"]
 });
 
+
+
 export const metadata = {
-  title: "Ryan Rumao",
-  description: "Portfolio of Ryan Rumao",
+  title: "Ryan Rumao - Portfolio",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className="scroll-smooth">
       <body
-        className={`${outfit.className} ${ovo.className} antialiased`}
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden dark:bg-darkTheme dark:text-white`}
       >
         {children}
       </body>
