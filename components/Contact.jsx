f266a37648a78620b053520a9c3573a33005ed29
rumao <rumao@ad.engr.wisc.edu>
2025-03-25 16:09:46 -0500

    Removed Gradient, Changed Resume Path

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -37,7 +37,7 @@ const Contact = () => {
     initial={{ opacity: 0 }} 
       whileInView={{ opacity: 1 }} 
       transition={{ duration: 1 }} 
-    id='contact' className='w-full px-[12%] py-10 scroll-mt-20 bg-[url("/footer-bg-color.png")] bg-no-repeat bg-center bg-[length:90%_auto] dark:bg-none'>
+    id='contact' className='w-full px-[12%] py-10 scroll-mt-20'>
 
       <motion.h4 
       initial={{ y: -20, opacity: 0 }} 
