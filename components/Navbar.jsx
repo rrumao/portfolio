bd297cbd851f26b561bfa3f43a29d40e1c3f0e5c
Ryan Rumao <ryanrumao4@gmail.com>
2025-03-26 21:02:02 -0500

    Merged hopefully

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -26,10 +26,6 @@ const Navbar = ({ isDarkMode, setIsDarkMode }) => {
 
   return (
     <>
-      {/* <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden">
-        <Image src={assets.header_bg_color} alt="" className="w-full" />
-      </div> */}
-
       <nav
         className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${
           isScroll
