6da3fda9c555edf5971f0d63e183b6f770dbaf7c
rumao <rumao@ad.engr.wisc.edu>
2025-03-31 12:23:57 -0500

    Finished Contact Us

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -1,34 +1,30 @@
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { motion } from "motion/react"
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
 
   const [result, setResult] = useState("");
+  const form = useRef();
 
   const onSubmit = async (event) => {
     event.preventDefault();
     setResult("Sending....");
-    const formData = new FormData(event.target);
 
-    // Enter your web3 froms access key below
-    formData.append("access_key", "------Enter Access Key Here-------");
-
-    const response = await fetch("https://api.web3forms.com/submit", {
-      method: "POST",
-      body: formData
-    });
-
-    const data = await response.json();
-
-    if (data.success) {
-      setResult("Form Submitted Successfully");
-      event.target.reset();
-    } else {
-      console.log("Error", data);
-      setResult(data.message);
-    }
+    emailjs
+      .sendForm('service_vlmb1ut', 'template_1omeoow', form.current, {
+        publicKey: '5wsqh_crA7kicvpek',
+      })
+      .then(
+        () => {
+          setResult('SUCCESS!');
+        },
+        (error) => {
+          setResult('FAILED...', error.text);
+        },
+      );
   };
 
 
@@ -64,21 +60,22 @@ const Contact = () => {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ delay: 0.9, duration: 0.5 }}
-      onSubmit={onSubmit} className='max-w-2xl mx-auto'>
+      onSubmit={onSubmit} className='max-w-2xl mx-auto'
+      ref={form}>
         <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
 
             <motion.input
             initial={{ x: -50, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ delay: 1.1, duration: 0.6 }}
-            type="text" placeholder='Enter your name' required
+            type="text" placeholder='Name' required
             className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='name'/>
 
             <motion.input
             initial={{ x: 50, opacity: 0 }}
             whileInView={{ x: 0, opacity: 1 }}
             transition={{ delay: 1.2, duration: 0.6 }}
-            type="email" placeholder='Enter your email' required
+            type="email" placeholder='Email' required
             className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white dark:bg-darkHover/30 dark:border-white/90' name='email'/>
 
         </div>
@@ -86,7 +83,7 @@ const Contact = () => {
         initial={{ y: 100, opacity: 0 }}
         whileInView={{ y: 0, opacity: 1 }}
         transition={{ delay: 1.3, duration: 0.6 }}
-        rows='6' placeholder='Enter your message' required
+        rows='6' placeholder='Message' required
         className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6 dark:bg-darkHover/30 dark:border-white/90' name='message'></motion.textarea>
 
         <motion.button
